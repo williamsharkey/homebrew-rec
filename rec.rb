02ad7f78a158e944f38d27e48434183ded22a6dd
William Sharkey <williamsharkey@gmail.com>
2019-06-30 00:24:59 -0400

    Update rec.rb

--- rec.rb
+++ rec.rb
@@ -1,7 +1,7 @@
 class Rec < Formula
   desc "Audio recorder for macOS terminal"
   homepage "https://github.com/williamsharkey/rec/"
-  url "https://github.com/williamsharkey/rec/releases/download/0.0.0/rec"
+  url "https://github.com/williamsharkey/rec/releases/download/0.1-alpha/rec"
   sha256 "ea7cb8d20a3660b2064a720d8868848c8bc2acb905fd2133b3a296c745df4c7b"
   depends_on "portaudio"
 
