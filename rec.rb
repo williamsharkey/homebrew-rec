f33426be383b0bdaf56977e4075c436e5919137e
William Sharkey <williamsharkey@gmail.com>
2019-06-29 23:57:55 -0400

    add homebrew formula for rec

---
+++ rec.rb
@@ -0,0 +1,12 @@
+class Rec < Formula
+  desc "Audio recorder for macOS terminal"
+  homepage "https://github.com/williamsharkey/rec/"
+  url "https://github.com/williamsharkey/rec/releases/download/0.0.0/rec"
+  sha256 "ea7cb8d20a3660b2064a720d8868848c8bc2acb905fd2133b3a296c745df4c7b"
+  depends_on "portaudio"
+
+  def install
+    bin.install "rec"
+  end
+
+end
